1a74ade0137d70f6e10aaff6035b55dea803d2b5
Shihab Uddin <piash222@gmail.com>
2019-10-06 14:53:46 +0600

    media file different folder for each listing

--- listings/models.py
+++ listings/models.py
@@ -5,6 +5,9 @@ from realtors.models import Realtor
 
 # Create your models here.
 class Listing(models.Model):
+    def get_upload_path(self, filename):
+        return f'documents/{0}/{1}'.format(self.title, filename)
+
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
@@ -18,13 +21,13 @@ class Listing(models.Model):
     garage = models.IntegerField(default=0)
     sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
-    photo_main = models.ImageField(upload_to="photos/%Y/%m/%d")
-    photo_1 = models.ImageField(upload_to="photos/%Y/%m/%d", blank=True)
-    photo_2 = models.ImageField(upload_to="photos/%Y/%m/%d", blank=True)
-    photo_3 = models.ImageField(upload_to="photos/%Y/%m/%d", blank=True)
-    photo_4 = models.ImageField(upload_to="photos/%Y/%m/%d", blank=True)
-    photo_5 = models.ImageField(upload_to="photos/%Y/%m/%d", blank=True)
-    photo_6 = models.ImageField(upload_to="photos/%Y/%m/%d", blank=True)
+    photo_main = models.ImageField(upload_to=get_upload_path)
+    photo_1 = models.ImageField(upload_to=get_upload_path, blank=True)
+    photo_2 = models.ImageField(upload_to=get_upload_path, blank=True)
+    photo_3 = models.ImageField(upload_to=get_upload_path, blank=True)
+    photo_4 = models.ImageField(upload_to=get_upload_path, blank=True)
+    photo_5 = models.ImageField(upload_to=get_upload_path, blank=True)
+    photo_6 = models.ImageField(upload_to=get_upload_path, blank=True)
     is_published = models.BooleanField(default=True)
     list_date = models.DateTimeField(default=datetime.now, blank=True)
 
