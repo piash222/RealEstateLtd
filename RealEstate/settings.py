32c05412a089b5f37c28b1e54ac2e165b0b2adaa
shihab uddin piash <piash222@gmail.com>
2020-01-14 13:47:18 +0600

    Update settings.py

--- RealEstate/settings.py
+++ RealEstate/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'realestate',
         'USER': 'postgres',
-        'PASSWORD': '20539255',
+        'PASSWORD': 'pass1234',
         "HOST": 'localhost'
     }
 }
